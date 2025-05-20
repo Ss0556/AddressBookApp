eef7572a62370c877f1e275fab14ed4c300a7efd
ss0556 <ss0556@srmist.edu.in>
2025-05-20 12:40:40 +0530

    UC4: Add repository and service interface

---
+++ src/main/java/com/example/AddressBook/repository/AddressBookRepository.java
@@ -0,0 +1,8 @@
+package com.example.AddressBook.repository;
+
+import com.example.AddressBook.model.AddressBookData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressBookRepository extends JpaRepository<AddressBookData, Integer> {
+}
+ 
